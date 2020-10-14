ba06a2e14cc87784764f25ae2de4ab27681010a1
Tom Tsiliopoulos <tsilipoulos@hotmail.com>
2020-10-14 16:15:48 -0400

    added full CRUD to component list

--- public/Scripts/app.js
+++ public/Scripts/app.js
@@ -4,6 +4,18 @@
     function Start()
     {
         console.log("App Started...");
+
+        let dangerButtons = document.getElementsByClassName("btn-danger");
+
+        for (const button of dangerButtons) {
+            button.addEventListener('click', (event) => {
+                if(!confirm("Are you sure?"))
+                {
+                    event.preventDefault();
+                    location.href = '/component-list';
+                }
+            });
+        }
     }
 
     window.addEventListener('load', Start);
--- routes/component.js
+++ routes/component.js
@@ -21,29 +21,83 @@ router.get('/', function(req, res, next) {
     
   });
   
-/* GET Add page. CREATE  */
+/* GET Display Add page. CREATE  */
 router.get('/add', (req, res, next)=> {
     res.render('index', { title: 'Add Component' });
 });
 
 /* POST process the Add page. CREATE */
 router.post('/add', (req, res, next)=> {
-    
+
+    // instantiate a new object of type Component
+    let component = Component.Model({
+        "partID":req.body.partID,
+        "name": req.body.name,
+        "description": req.body.description,
+        "price": req.body.price
+    });
+
+    Component.Model.create(component, (err, Component) => {
+        if(err)
+        {
+            console.log(err);
+            res.end(err);
+        }
+        res.redirect('/component-list');
+    });
 });
 
-/* GET Edit page. UPDATE */
+/* GET Display Edit page. UPDATE */
 router.get('/edit/:id', (req, res, next)=> {
+    let id = req.params.id;
 
+    // pass id to the db 
+    Component.Model.findById(id, (err, ComponentToEdit) => {
+        if(err)
+        {
+            console.log(err);
+            res.end(err);
+        }
+        // show the edit view
+        res.render('index', { title: 'Edit Component', data: ComponentToEdit });
+    });
 });
 
 /* POST process the Edit page. UPDATE */
 router.post('/edit/:id', (req, res, next)=> {
+    let id = req.params.id;
 
+     // instantiate a new object of type Component
+     let updatedComponent = Component.Model({
+        "_id": id, 
+        "partID":req.body.partID,
+        "name": req.body.name,
+        "description": req.body.description,
+        "price": req.body.price
+    });
+
+    Component.Model.updateOne({_id: id}, updatedComponent, (err) => {
+        if(err)
+        {
+            console.log(err);
+            res.end(err);
+        }
+        res.redirect('/component-list');
+    });
 });
 
 /* GET process the Delete page. DELETE */
 router.get('/delete/:id', (req, res, next)=> {
+    let id = req.params.id;
 
+    Component.Model.remove({_id: id}, (err) => {
+        if(err)
+        {
+            console.log(err);
+            res.end(err);
+        }
+        res.redirect('/component-list');
+    });
 });
 
 
